16cd230299e56369aa210807d26b64a45944814d
Aarnav Tale <aarnav@tale.me>
2021-11-14 16:06:32 -0500

    feat: remove unnecessary loading bounce

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,19 +1,8 @@
 <script lang="ts">
 	import Lazy from 'svelte-lazy';
-
-	function removeSpinner() {
-		setTimeout(() => {
-			document.getElementsByClassName('spinner').item(0).remove();
-		}, 500);
-	}
 </script>
 
-<div class="spinner">
-	<div class="double-bounce1"></div>
-	<div class="double-bounce2"></div>
-</div>
-
-<Lazy onload="{removeSpinner}">
+<Lazy>
 	<div id="background"></div>
 	<div id="overlay"></div>
 </Lazy>
@@ -79,58 +68,6 @@
 		#overlay {
 			background-color: rgba(0, 0, 0, 0.25);
 			opacity: 0.75;
-		}
-	}
-
-	.spinner {
-		top: 50%;
-		left: 50%;
-		width: 40px;
-		height: 40px;
-		position: fixed;
-		margin: auto auto;
-		transform: translate(-50%, -50%);
-	}
-
-	.double-bounce1,
-	.double-bounce2 {
-		width: 100%;
-		height: 100%;
-		border-radius: 50%;
-		background-color: #333;
-		opacity: 0.6;
-		position: absolute;
-		top: 0;
-		left: 0;
-
-		-webkit-animation: sk-bounce 2s infinite ease-in-out;
-		animation: sk-bounce 2s infinite ease-in-out;
-	}
-
-	.double-bounce2 {
-		-webkit-animation-delay: -1s;
-		animation-delay: -1s;
-	}
-
-	@-webkit-keyframes sk-bounce {
-		0%,
-		100% {
-			-webkit-transform: scale(0);
-		}
-		50% {
-			-webkit-transform: scale(1);
-		}
-	}
-
-	@keyframes sk-bounce {
-		0%,
-		100% {
-			transform: scale(0);
-			-webkit-transform: scale(0);
-		}
-		50% {
-			transform: scale(1);
-			-webkit-transform: scale(1);
 		}
 	}
 </style>
