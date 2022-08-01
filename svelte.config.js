b868a1d86043bed1f2114f1e8dce606d3a67b5d4
Aarnav Tale <aarnav@tale.me>
2022-08-01 11:57:22 -0400

    fix: go back to the static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,14 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 	kit: {
-		adapter: adapter({
-			edge: false,
-			external: [],
-			split: false
-		})
+		adapter: adapter(),
+		prerender: {
+			default: true
+		}
 	}
 };
 
