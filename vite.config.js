4cea79db732162a45d53478c52b4d50e8720ac99
Aarnav Tale <aarnav@tale.me>
2022-08-01 11:58:55 -0400

    fix: resolve sveltekit build issue

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			// https://github.com/octokit/octokit.js/issues/2126#issuecomment-1186210471
+			'node-fetch': 'isomorphic-fetch',
+		},
+	}
 };
 
 export default config;
