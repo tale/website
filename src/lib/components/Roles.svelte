7d6a3a704d850341bce417c40fade7fb4b553df3
Aarnav Tale <aarnavtale@icloud.com>
2023-03-09 19:14:05 -0500

    fix: don't cut layout off on mobile

--- src/lib/components/Roles.svelte
+++ src/lib/components/Roles.svelte
@@ -1,43 +1,40 @@
-<script>
+<script lang='ts'>
 	const roles = [
 		{
 			name: 'Software Engineer',
 			link: 'https://chariz.org',
 			place: 'Chariz',
 			time: '2021 - Present',
-			description:
-				'Using Kubernetes and containerization, I attained the responsibility of migrating our systems to containers. I am responsible for deploying changes to our new infrastructure, managing the health of our clusters, and maintaining reliability on services accessed by users.',
+			description: 'I\'m a software engineer at Chariz, a startup that\'s building a platform for iOS developers to monetize their products. I\'m currently working on the backend of the platform, building out features to provide the best experience for our users.'
 		},
 		{
-			name: 'Computer Science Student',
+			name: 'University Student',
 			link: 'https://gmu.edu/',
 			place: 'George Mason University',
-			time: '2022 - 2026',
-			description:
-				"I'm currently studying for a Bachelor of Science degree in Computer Science and a minor in Software Engineering (SWE). I also participate in various debate and writing related student organizations on campus.",
+			time: '2022 - Present',
+			description: 'I\'m a full-time student at George Mason University, studying for a Bachelor of Science in Computer Science and a minor in Software Engineering. I\'m also taking courses to further my interest in architecture and calligraphy/typography.'
 		}
 	];
+
+	let innerWidth: number;
 </script>
+
+<svelte:window bind:innerWidth />
 
 <h2>Experience</h2>
 <div>
 	{#each roles as role}
-		<div>
-			{#if role.link}
+		<div class="meta">
+			{#if innerWidth <= 425}
 				<span>{role.name}</span>
-				<p>
-					<a href="{role.link}">@{role.place}</a> —
-					{role.time}
-				</p>
+				<p>@<a href="{role.link}">{role.place}</a></p>
+				<p>{role.time}</p>
 			{:else}
-				<span>{role.name}</span>
-				<p>
-					@{role.place} —
-					{role.time}
-				</p>
+				<p><span>{role.name}</span>{' @ '}<a href="{role.link}">{role.place}</a></p>
+				<p>{role.time}</p>
 			{/if}
 		</div>
-		<p>{role.description}</p>
+		<p class="spaced">{role.description}</p>
 		<br />
 	{/each}
 </div>
@@ -50,5 +47,25 @@
 	span {
 		font-style: oblique;
 		font-size: large;
+	}
+
+	p {
+		vertical-align: middle;
+	}
+
+	.spaced {
+		margin-top: 6px;
+	}
+
+	.meta {
+		display: flex;
+		justify-content: space-between;
+	}
+
+
+	@media (max-width: 425px) {
+		.meta {
+			display: initial;
+		}
 	}
 </style>
