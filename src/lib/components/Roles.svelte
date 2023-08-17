9a2d9ebd3002a8698d47b1059893a879640afda2
Aarnav Tale <aarnavtale@icloud.com>
2023-08-17 02:42:14 -0400

    chore: update roles

--- src/lib/components/Roles.svelte
+++ src/lib/components/Roles.svelte
@@ -1,19 +1,29 @@
-<script lang='ts'>
+<script lang="ts">
 	const roles = [
 		{
-			name: 'Software Engineer',
-			link: 'https://chariz.org',
-			place: 'Chariz',
-			time: '2021 - Present',
-			description: 'I\'m a software engineer at Chariz, a startup that\'s building a platform for iOS developers to monetize their products. I\'m currently working on the backend of the platform, building out features to provide the best experience for our users.'
+			name: "Software Engineer",
+			link: "https://chariz.org",
+			place: "Chariz",
+			time: "2021 - Present",
+			description:
+				"On top of managing company operations in the United States, I also work on the backend of the Chariz platform, building out features to provide the best experience for our sellers and  users.",
 		},
 		{
-			name: 'University Student',
-			link: 'https://gmu.edu/',
-			place: 'George Mason University',
-			time: '2022 - Present',
-			description: 'I\'m a full-time student at George Mason University, studying for a Bachelor of Science in Computer Science and a minor in Software Engineering. I\'m also taking courses to further my interest in architecture and calligraphy/typography.'
-		}
+			name: "Computer Science Student",
+			link: "https://gmu.edu/",
+			place: "George Mason University",
+			time: "2022 - Present",
+			description:
+				"I'm studying towards my Bachelor of Science in Computer Science. I've also involved myself in various other activities on campus including research and teaching.",
+		},
+		{
+			name: "Teaching Assistant",
+			link: "https://cs.gmu.edu/",
+			place: "George Mason University",
+			time: "2023 - Present",
+			description:
+				"I'm a teaching assistant for CS262: Introduction to Low-level Programming, a fundamentals oriented class that focuses on teaching students how to use C and work with hardware.",
+		},
 	];
 
 	let innerWidth: number;
@@ -27,10 +37,14 @@
 		<div class="meta">
 			{#if innerWidth <= 425}
 				<span>{role.name}</span>
-				<p>@<a href="{role.link}">{role.place}</a></p>
+				<p>@<a href={role.link}>{role.place}</a></p>
 				<p>{role.time}</p>
 			{:else}
-				<p><span>{role.name}</span>{' @ '}<a href="{role.link}">{role.place}</a></p>
+				<p>
+					<span>{role.name}</span>{" @ "}<a href={role.link}
+						>{role.place}</a
+					>
+				</p>
 				<p>{role.time}</p>
 			{/if}
 		</div>
@@ -41,7 +55,7 @@
 
 <style>
 	h2 {
-		font-family: 'DM Serif Display', serif;
+		font-family: "DM Serif Display", serif;
 	}
 
 	span {
@@ -61,7 +75,6 @@
 		display: flex;
 		justify-content: space-between;
 	}
-
 
 	@media (max-width: 425px) {
 		.meta {
