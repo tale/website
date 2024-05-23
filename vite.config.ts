62abecffd9d0ffb79f1f64e2631645d45cc6b8c6
Aarnav Tale <aarnavtale@icloud.com>
2024-05-23 12:47:29 -0400

    fix: resolve dev mode issues and use react 19 compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,32 @@
-import { vitePlugin as remix } from '@remix-run/dev'
+import {
+	cloudflareDevProxyVitePlugin as devProxy,
+	vitePlugin as remix,
+} from '@remix-run/dev'
 import { defineConfig } from 'vite'
+import babel from 'vite-plugin-babel'
 import svgr from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
 	plugins: [
-		remix(),
+		devProxy(),
+		remix({
+			future: {
+				v3_fetcherPersist: true,
+				v3_relativeSplatPath: true,
+				v3_throwAbortReason: true,
+			},
+		}),
 		tsconfigPaths(),
+		babel({
+			filter: /\.[jt]sx?$/,
+			babelConfig: {
+				presets: ['@babel/preset-typescript'],
+				plugins: [
+					['babel-plugin-react-compiler', {}],
+				],
+			},
+		}),
 		svgr({
 			include: '**/*.svg',
 			svgrOptions: {
