05f874e25da2b8f6bd95311bed831d2ed79c0f82
Aarnav Tale <aarnavtale@icloud.com>
2024-06-17 11:02:03 -0400

    feat: add blog

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
 import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://tale.me',
-	integrations: [tailwind(), icon()]
+	integrations: [tailwind(), icon(), mdx()],
 });
