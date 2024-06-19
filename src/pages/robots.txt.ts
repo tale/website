2546cf78016d759543f39f06252972fc5677444f
Aarnav Tale <aarnavtale@icloud.com>
2024-06-19 09:29:53 -0400

    chore: make robots.txt dynamically generated

---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,14 @@
+const robots = `
+User-agent: *
+Allow: /
+
+Sitemap: ${new URL('sitemap-index.xml', import.meta.env.SITE).href}
+`.trim()
+
+export function GET() {
+	return new Response(robots, {
+		headers: {
+			'Content-Type': 'text/plain; charset=utf-8',
+		}
+	})
+}
