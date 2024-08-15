669d58395d4da0b74c79b1438ac508e20fde497e
Aarnav Tale <aarnavtale@icloud.com>
2024-08-15 10:43:50 -0400

    chore: add rss

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,18 @@
+import rss from "@astrojs/rss"
+import { getCollection } from "astro:content"
+
+export async function GET(context) {
+	const posts = await getCollection('blog')
+	return rss({
+		title: "Aarnav Tale's Blog",
+		description: 'Just a collection of thoughts, ideas, and technical notes.',
+		customData: `<language>en-us</language>`,
+		site: context.site,
+		items: posts.map(post => ({
+			title: post.data.title,
+			description: post.data.description,
+			pubDate: post.data.date,
+			link: `/blog/${post.slug}`,
+		}))
+	});
+}
