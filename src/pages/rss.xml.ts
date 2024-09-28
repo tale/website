13f312e5c2be2bd2d9e4e6c34832159e6203af7e
Aarnav Tale <aarnavtale@icloud.com>
2024-09-28 17:02:55 -0400

    fix: build works & deps are updated

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,13 +1,14 @@
 import rss from "@astrojs/rss"
 import { getCollection } from "astro:content"
+import type { APIContext } from 'astro'
 
-export async function GET(context) {
+export async function GET(context: APIContext) {
 	const posts = await getCollection('blog')
 	return rss({
 		title: "Aarnav Tale's Blog",
 		description: 'Just a collection of thoughts, ideas, and technical notes.',
 		customData: `<language>en-us</language>`,
-		site: context.site,
+		site: context.site ?? '',
 		items: posts.map(post => ({
 			title: post.data.title,
 			description: post.data.description,
